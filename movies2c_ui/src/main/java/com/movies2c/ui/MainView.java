ea1a449d0cb1c50504289f3a7ed8aec067996b92
kreeeks <giannis.krikis50@gmail.com>
2025-11-23 21:59:16 +0200

    title and color

--- movies2c_ui/src/main/java/com/movies2c/ui/MainView.java
+++ movies2c_ui/src/main/java/com/movies2c/ui/MainView.java
@@ -3,11 +3,31 @@ package com.movies2c.ui;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new H1("Movies2C UI is running!"));
+        //edw asxolithika me to xrwma kai me ton titlo
+        setSizeFull();
+        setPadding(false);
+        setSpacing(false);
+        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        getStyle()
+
+                .set("background-color", "#050509")
+                .set("color","#f5f5ff")
+                .set("font-family", "system-ui");
+        H1 title = new H1("Movies2c");
+        title.getStyle()
+                .set("color", "#ffc857")
+                .set("margin-top", "20px")
+                .set("font-size", "40px")
+                .set("letter-spacing", "0.12em")
+                .set("text-transform", "uppercase");
+        add(title);
+
     }
 }
