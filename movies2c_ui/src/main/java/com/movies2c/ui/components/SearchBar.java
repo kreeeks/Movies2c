5a09ee9c87cc585aae9c6d9abc78482ed02d50b2
kreeeks <giannis.krikis50@gmail.com>
2025-11-25 18:12:44 +0200

    search

---
+++ movies2c_ui/src/main/java/com/movies2c/ui/components/SearchBar.java
@@ -0,0 +1,8 @@
+package com.movies2c.ui.components;
+
+public class SearchBar {
+    //vazeis ta katallhla imports kai grafeis  ton kwdika,kai meta anoigeis to mainview kai porstheteis
+    //SearchBar searchBar = new SearchBar();
+    //add(searchBar);
+
+}
