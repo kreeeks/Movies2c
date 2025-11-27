fec328a7112d6942b13728730067c0fc01980fe3
danae5302 <dan.sakkou@gmail.com>
2025-11-27 15:06:42 +0200

    database_connection

--- movies2c_backend/src/main/java/com/movies2c/backend/model/Quiz.java
+++ movies2c_backend/src/main/java/com/movies2c/backend/model/Quiz.java
@@ -1,4 +1,19 @@
 package com.movies2c.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Quiz {
+    private String id;
+    private String title;
+    private String  description;
+    private String questions;
+    private String answers;
 }
